d24a0c201574d2ce706f231fa3c44b5d7fc2b23b
geoffroyvilain <vilaingeoffroy02@gmail.com>
2019-01-24 15:55:06 +0100

    xo corrigé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,35 @@
-function getNumber() {
-var pointure = document.getElementById('pointure').value;
-var annee = document.getElementById('annee').value;
-if (isNaN(pointure)==false && isNaN(annee)==false) {
-              alert(((((pointure*2)+5)*50)-1985)+1766);
-            }else{
-              alert('Merci de renseigner les champs!');
+function calcul() {
+  var shoeSize = document.getElementById('shoeSize').value;
+  var yearOfBirth = document.getElementById('yearOfBirth').value;
+  // on verifie que les valeurs rentrées par l'utilisateur soient bien des nombres
+  if(!isNaN(shoeSize) && !isNaN(yearOfBirth)){
+    //on autorise la pointure entre 34 ET 52  ---- on autorise les années entre 1900 et 2019 comprises
+    if((shoeSize >= 34 && shoeSize <= 52) && (yearOfBirth >= 1900 && yearOfBirth <= 2019)) {   //  connecteur logique : ||=double pipe = ou / &&=et
+      var result = (((((shoeSize * 2)+5)*50)-yearOfBirth)+1766);/*ou deuxieme methode pour calcul :
+      var result = shoeSize * 2;
+      result +=5;
+      result *= 50;
+      result -= yearOfBirth;
+      result += 1766;
+      */
+      alert(result);
+    }else{
+      alert('La pointure et/ou la date de naissance ne sont pas des valeurs correctes');
+    }
+  }else{
+    alert('Il faut saisir des chiffres');
+  }
+
+}
+
+
+
+
+
+/*if (isNaN(shoeSize)==false && isNaN(yearOfBirth)==false) {
+alert(((((shoeSize*2)+5)*50)-1985)+1766);
+}else{
+alert('Merci de renseigner les champs!');
 }
 }
+*/
